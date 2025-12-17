7318b0c0612a255ecb31f71c71b4596f06666c61
Karl Marx Roxas <karlm.roxas@gmail.com>
2025-12-18 00:48:34 +0800

    chore: Add CORS config and minor changes

--- src/main/java/com/rainydaysengine/rainydays/application/service/user/UserLoginResponse.java
+++ src/main/java/com/rainydaysengine/rainydays/application/service/user/UserLoginResponse.java
@@ -2,13 +2,8 @@ package com.rainydaysengine.rainydays.application.service.user;
 
 import lombok.Builder;
 
-import java.time.OffsetDateTime;
-import java.util.Map;
-
 @Builder
 public record UserLoginResponse(
-        Map<String, Object> traits,
-        String token,
-        OffsetDateTime expiry
+        String tokenSession
 ) {
 }
